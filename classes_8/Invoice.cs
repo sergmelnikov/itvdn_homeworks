a850b7d4ea160193a7c43a16ae964d05c85075db
sergmelnikov <melnikhov@gmail.com>
2020-06-30 12:08:35 +0300

    Added classes_8 homework. Last in Lesson 2

---
+++ classes_8/Invoice.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace classes_8
+{
+    public class Invoice
+    {
+        private readonly int account;
+        private readonly string customer, provider;
+
+        public int Account { get; }
+        public string Customer
+        {
+            get
+            {
+                return customer;
+            }
+        }
+
+        public string Provider { get; }
+
+        private string article;
+        private int quantity;
+
+        public string Article
+        {
+            set 
+            {
+                if (value != null)
+                {
+                    article = value;
+                }
+            }
+            get
+            {
+                return article;
+            }
+        }
+
+        public int Quantity
+        { 
+            set
+            {
+                if (value > 0)
+                {
+                    quantity = value;
+                }
+            }
+            get
+            {
+                return quantity;
+            }
+        }
+
+        public void TaxCalculator()
+        {
+            double price = 0;
+            switch (article)
+            {
+                case "iPhone":
+                    price = 699;
+                    break;
+                case "MacBook":
+                    price = 999;
+                    break;
+                case "iMac":
+                    price = 1299;
+                    break;
+                default:
+                    Console.WriteLine("Unavailable article");
+                    break;
+            }
+            price *= quantity;
+            Console.WriteLine($"Price: ${price}. With tax: ${price * 1.2}");
+        }
+
+        public Invoice(int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+    }
+}
---
+++ classes_8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace classes_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice2321 = new Invoice(31232, "Dmitry Medvedev", "Swift");
+            invoice2321.Article = "iPhone";
+            invoice2321.Quantity = 10;
+            Console.WriteLine($"Customer: {invoice2321.Customer}");
+            Console.WriteLine($"Article: {invoice2321.Article}");
+            Console.WriteLine($"Quantity: {invoice2321.Quantity}");
+            invoice2321.TaxCalculator();
+        }
+    }
+}
